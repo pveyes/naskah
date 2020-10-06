1563c1eb483775d9ada0d0a7f65e67624e826b42
Fatih Kalifa <fatihkalifa@gmail.com>
2020-10-07 01:02:30 +0700

    refactor & fix demo

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,22 +1,20 @@
-#[macro_use]
-extern crate yew;
 extern crate printer;
-extern crate stdweb;
-#[macro_use]
-extern crate serde_derive;
+extern crate wasm_bindgen;
+extern crate web_sys;
+extern crate yew;
 
-pub mod worker;
-
-use printer::to_js;
+use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
-pub struct Model {
+use printer::to_js;
+
+struct Model {
+    link: ComponentLink<Self>,
     code: String,
     transpiled: String,
-    worker: Box<Bridge<worker::Worker>>,
 }
 
-pub enum Msg {
+enum Msg {
     ChangeCode(String),
     CompilationFinished(String),
 }
@@ -32,45 +30,38 @@ jika y benar {
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
-
-    fn create(_: Self::Properties, mut link: ComponentLink<Self>) -> Self {
-        let callback = link.send_back(|re| match re {
-            worker::Response::Compiled(t) => Msg::CompilationFinished(t),
-        });
-        let worker = worker::Worker::bridge(callback);
-
-        Model {
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
             code: EXAMPLE_CODE.into(),
             transpiled: to_js(EXAMPLE_CODE.into()),
-            worker,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::ChangeCode(code) => {
-                self.code = code.clone();
-                self.worker.send(worker::Request::Compile(code));
+            Msg::ChangeCode(v) => {
+                self.transpiled = to_js(v.clone().into());
+                self.code = v;
             }
-            Msg::CompilationFinished(t) => {
-                self.transpiled = t;
-            }
-        };
+            Msg::CompilationFinished(result) => self.transpiled = result,
+        }
         true
     }
-}
 
-impl Renderable<Model> for Model {
-    fn view(&self) -> Html<Self> {
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
         html! {
             <>
                 <div>
                     <label for="input",>{"Masukan"}</label>
-                    <textarea
-                        id="input",
-                        value=&self.code,
-                        oninput=|e| Msg::ChangeCode(e.value),>
-                    </textarea>
+                    <textarea value={&self.code} oninput={self.link.callback(|e: InputData| Msg::ChangeCode(e.value))} />
                 </div>
                 <div>
                     <label>{"Keluaran (JavaScript):"}</label>
@@ -78,5 +69,14 @@ impl Renderable<Model> for Model {
                 </div>
             </>
         }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    let win = web_sys::window().unwrap();
+    let doc = win.document().unwrap();
+    if let Some(element) = doc.query_selector("#playground").expect("No matching id") {
+        App::<Model>::new().mount(element);
     }
 }
--- scripts/build-demo.sh
+++ scripts/build-demo.sh
@@ -1,16 +1,5 @@
-## compile both entry point and worker file
-## specify target because by default it uses emscripten
+rustup default nightly
+cargo install wasm-pack
+cargo build
 cd demo
-cargo web build --bin main --target=wasm32-unknown-unknown --release
-cargo web build --bin worker --target=wasm32-unknown-unknown --release
-
-## copy compiled code to static dir to be served
-cd ..
-cp target/wasm32-unknown-unknown/release/main.js demo/static/
-cp target/wasm32-unknown-unknown/release/main.wasm demo/static/
-
-# copy worker code, remove symlink for dev env
-rm -rf demo/static/worker
-mkdir -p demo/static/worker
-cp target/wasm32-unknown-unknown/release/worker.js demo/static/worker/
-cp target/wasm32-unknown-unknown/release/worker.wasm demo/static/worker/
+wasm-pack build --target web --out-name wasm --out-dir ./static/assets
